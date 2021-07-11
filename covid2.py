3168e08f531e423403a391be68b9cbeda385ed9c
adhitio-github <70420768+adhitio-github@users.noreply.github.com>
2021-07-11 17:53:40 +0700

    Update covid2.py

--- covid2.py
+++ covid2.py
@@ -1,16 +1,24 @@
 import streamlit as st
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
 import itertools
 import os
 #import matplotlib.pylab as plt
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
-#import streamlit.components.v1 as stc
+import streamlit.components.v1 as stc
+
+import io
+file_buffer = st.file_uploader(...)
+text_io = io.TextIOWrapper(file_buffer)
+
+from tensorflow.keras.models import load_model
 
 # File Processing Pkgs
 import pandas as pd
 #import docx2txt
-from PIL import Image 
+from PIL import Image  
 #from PyPDF2 import PdfFileReader
 #import pdfplumber
 
